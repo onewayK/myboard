43af126dfe69b984624137f08a102cb4436d7397
oneway <oneway@codemind.co.kr>
2024-06-27 15:59:50 +0900

    로그인 구현하기

--- users/serializers.py
+++ users/serializers.py
@@ -1,22 +1,31 @@
-from django.contrib.auth.models import User # User 모델
-from django.contrib.auth.password_validation import validate_password # Django의 기본 패스워드 검증 도구
+# Django의 기본 authenticate 함수, 우리가 설정한 DefaultAuthBackend인 TokenAuth 방식으로 유저를 인증해줌
+from django.contrib.auth import authenticate
+# User 모델
+from django.contrib.auth.models import User
+# Django의 기본 패스워드 검증 도구
+from django.contrib.auth.password_validation import validate_password
 
 from rest_framework import serializers
-from rest_framework.authtoken.models import Token # Token 모델
-from rest_framework.validators import UniqueValidator # 이메일 중복 방지를 위한 검증 도구
+# Token 모델
+from rest_framework.authtoken.models import Token
+# 이메일 중복 방지를 위한 검증 도구
+from rest_framework.validators import UniqueValidator
 
 # 회원가입 시리얼라이저
 class RegisterSerializer(serializers.ModelSerializer):
     email = serializers.EmailField(
         required=True,
-        validators=[UniqueValidator(queryset=User.objects.all())], # 이메일에 대한 중복 검증
+        # 이메일에 대한 중복 검증
+        validators=[UniqueValidator(queryset=User.objects.all())],
     )
     password = serializers.CharField(
         write_only=True,
         required=True,
-        validators=[validate_password], # 비밀번호에 대한 검증
+        # 비밀번호에 대한 검증
+        validators=[validate_password],
     )
-    password2 = serializers.CharField(write_only=True, required=True) # 비밀번호 확인을 위한 필드
+    # 비밀번호 확인을 위한 필드
+    password2 = serializers.CharField(write_only=True, required=True)
 
     class Meta:
         model = User
@@ -41,3 +50,19 @@ class RegisterSerializer(serializers.ModelSerializer):
         user.save()
         token = Token.objects.create(user=user)
         return user
+
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only=True)
+    # write_only 옵션을 통해 클라이언트 -> 서버 방향의 역직렬화는 가능, 서버 -> 클라이언트 방향의 직렬화는 불가능
+
+    def validate(self, data):
+        user = authenticate(**data)
+        if user:
+            # 토근에서 유저 찾아 응답
+            token = Token.objects.get(user=user)
+            return token
+        raise serializers.ValidationError(
+            {"error": "Unable to log in with provided credentials."}
+        )
--- users/views.py
+++ users/views.py
@@ -1,10 +1,21 @@
 from django.contrib.auth.models import User
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.response import Response
 
-from .serializers import RegisterSerializer
+from .serializers import RegisterSerializer, LoginSerializer
 
 # CreateAPIView(generics) 사용 구현
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
-    
+
+
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
+
+    def post(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        # validate()의 리턴값인 Token을 받아옴.
+        token = serializer.validated_data
+        return Response({"token": token.key}, status=status.HTTP_200_OK)
