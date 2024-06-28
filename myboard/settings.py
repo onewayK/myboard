b789208021d3fb4617c1fdcc7925d6f2d67f65d9
oneway <oneway@codemind.co.kr>
2024-06-28 11:34:06 +0900

    [게시글] 필터링

--- myboard/settings.py
+++ myboard/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'users',
     'posts',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
@@ -108,7 +109,10 @@ AUTH_PASSWORD_VALIDATORS = [
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
-    ]
+    ],
+    'DEFAULT_FILTER_BACKENDS': [
+        'django_filters.rest_framework.DjangoFilterBackend',
+    ],
 }
 
 
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,5 @@
+from django_filters.rest_framework import DjangoFilterBackend
+
 from rest_framework import viewsets
 
 from users.models import Profile
@@ -18,4 +20,18 @@ class PostViewSet(viewsets.ModelViewSet):
     def perform_create(self, serializer):
         profile = Profile.objects.get(user=self.request.user)
         serializer.save(author=self.request.user, profile=profile)
-        
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    permission_classes = [CustomReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author', 'likes']
+
+    def get_serializer_class(self):
+        if self.action == 'list' or 'retrieve':
+            return PostSerializer
+        return PostCreateSerializer
+
+    def perform_create(self, serializer):
+        profile = Profile.objects.get(user=self.request.user)
+        serializer.save(author=self.request.user, profile=profile)
