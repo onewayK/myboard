2d425995ff463a020177ac9745434c64616bd041
oneway <oneway@codemind.co.kr>
2024-06-28 13:44:19 +0900

    [댓글] 뷰

--- posts/views.py
+++ posts/views.py
@@ -7,14 +7,16 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 
 from users.models import Profile
-from .models import Post
+from .models import Post, Comment
 from .permissions import CustomReadOnly
-from .serializers import PostSerializer, PostCreateSerializer
+from .serializers import PostSerializer, PostCreateSerializer, CommentSerializer, CommentCreateSerializer
 
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     permission_classes = [CustomReadOnly]
+    filter_backends = [DjangoFilterBackend]
+    filterset_fields = ['author', 'likes']
 
     def get_serializer_class(self):
         if self.action == 'list' or 'retrieve':
@@ -67,3 +69,17 @@ def like_post(request, pk):
         post.likes.add(request.user)
 
     return Response({'status': 'ok'})
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    queryset = Comment.objects.all()
+    permission_classes = [CustomReadOnly]
+
+    def get_serializer_class(self):
+        if self.action == 'list' or 'retrieve':
+            return CommentSerializer
+        return CommentCreateSerializer
+
+    def perform_create(self, serializer):
+        profile = Profile.objects.get(user=self.request.user)
+        serializer.save(author=self.request.user, profile=profile)
